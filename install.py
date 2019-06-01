3577f9e844163fa7036cf3fc3ba4117aac6cd92f
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2019-06-02 01:44:17 +0700

    fix tinggal management-kubeconfig

--- install.py
+++ install.py
@@ -16,7 +16,7 @@ def hosts_generate():
     with open("./hosts/hosts", "w") as fh:
         fh.write(template.render(config_data))
 
-def tasks_generate():
+def main_tasks_generate():
     #Import necessary functions from Jinja2 module
     from jinja2 import Environment, FileSystemLoader
 
@@ -38,7 +38,102 @@ def tasks_generate():
     #delete "," in all line if exist
     os.system("sed -i 's/,$//' main.yml")
 
+def master_setup_bootstraping():
+    #Import necessary functions from Jinja2 module
+    from jinja2 import Environment, FileSystemLoader
+
+    #Import YAML module
+    import yaml
+
+    #Load data from YAML into Python dictionary
+    config_data = yaml.load(open('./group_vars/all.yml'))
+
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./roles/master-setup/tasks/'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('01-bootstraping.yml.j2')
+
+    #Render the template with data and print the output
+    with open("./roles/master-setup/tasks/01-bootstraping.yml", "w") as fh:
+        fh.write(template.render(config_data))
+
+
+def worker_setup_bootstraping():
+    #Import necessary functions from Jinja2 module
+    from jinja2 import Environment, FileSystemLoader
+
+    #Import YAML module
+    import yaml
+
+    #Load data from YAML into Python dictionary
+    config_data = yaml.load(open('./group_vars/all.yml'))
+
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./roles/worker-setup/tasks/'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('01-bootstraping.yml.j2')
+
+    #Render the template with data and print the output
+    with open("./roles/worker-setup/tasks/01-bootstraping.yml", "w") as fh:
+        fh.write(template.render(config_data))
+
+def flannel():
+    #Import necessary functions from Jinja2 module
+    from jinja2 import Environment, FileSystemLoader
+
+    #Import YAML module
+    import yaml
+
+    #Load data from YAML into Python dictionary
+    config_data = yaml.load(open('./group_vars/all.yml'))
+
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./roles/flannel/tasks/'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('flannel.yml.j2')
+
+    #Render the template with data and print the output
+    with open("./roles/flannel/tasks/flannel.yml", "w") as fh:
+        fh.write(template.render(config_data))
+
+def calico():
+    #Import necessary functions from Jinja2 module
+    from jinja2 import Environment, FileSystemLoader
+
+    #Import YAML module
+    import yaml
+
+    #Load data from YAML into Python dictionary
+    config_data = yaml.load(open('./group_vars/all.yml'))
+
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./roles/calico/tasks/'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('calico.yml.j2')
+
+    #Render the template with data and print the output
+    with open("./roles/calico/tasks/calico.yml", "w") as fh:
+        fh.write(template.render(config_data))
+
+def deployer_kubeconfig():
+    #Import necessary functions from Jinja2 module
+    from jinja2 import Environment, FileSystemLoader
+
+    #Import YAML module
+    import yaml
+
+    #Load data from YAML into Python dictionary
+    config_data = yaml.load(open('./group_vars/all.yml'))
+
+    #Load Jinja2 template
+    env = Environment(loader = FileSystemLoader('./roles/deployer-kubeconfig/tasks/'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('01-copy.yml.j2')
+
+    #Render the template with data and print the output
+    with open("./roles/deployer-kubeconfig/tasks/01-copy.yml", "w") as fh:
+        fh.write(template.render(config_data))
 
 if __name__ == "__main__":
     hosts_generate()
-    tasks_generate()
+    main_tasks_generate()
+    master_setup_bootstraping()
+    worker_setup_bootstraping()
+    flannel()
+    calico()
+    deployer_kubeconfig()
